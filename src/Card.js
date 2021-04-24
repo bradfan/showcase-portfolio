3d72c4be37adb6ecc8b5c8e60e2744f50a830c66
jcerb <brt.jst@gmail.com>
2021-04-24 14:03:53 -0700

    fill in card.js, adjust projects.json

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+
+const styles = {
+    // display: "flex",
+    // flexDirection: "row",
+    // float: "left",
+    // width: "33%",
+    padding: "0 10px",
+    border: "3px solid black",
+    borderRadius: "10px",
+    
+  };
 
 function Card() {
-    return (
-        <div>
-            
+  return (
+    <div>
+
+      <div className="card">
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
         </div>
-    )
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Name:</strong> {props.name}
+            </li>
+            <li>
+              <strong>Github Repo Link:</strong> {props.github}
+            </li>
+            <li>
+              <strong>Deployed Link:</strong> {props.deployed}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  );
 }
 
-export default Card
+export default Card;
