ad9d57dde726da7f29c56f066cccd076d5b34491
jcerb <brt.jst@gmail.com>
2021-04-26 09:38:29 -0700

    link card(props) to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import projects from "./projects.json";
 import Header from "./Header";
 import Card from "./Card";
 import Footer from "./Footer";
 
 
-function App() {
+class App extends Component {
+  state = {
+    projects
+  };
 
 
 
-
-  return (
-    <div>
-      <Header></Header>
-      
-      {/* <Card></Card> */}
-      
-     <Footer></Footer>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        {this.state.projects.map(project => (
+        <Card
+        name={project.name}
+        image={project.image}
+        github={project.github}
+        deployed={project.deployed}
+        />
+        ))}
+        <Footer></Footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
