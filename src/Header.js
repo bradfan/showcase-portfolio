4ae86754bebb0af53f8827956f00749d41688869
jcerb <brt.jst@gmail.com>
2021-04-26 17:39:52 -0700

    add links to projectsjson

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
 import background from "./images/hero.jpg";
+//  import personal from "./images/personal.png";
 
 const styles = {
   fontFamily: "Roboto, Times New Roman, Times, serif",
   boxSizing: "border-box",
   backgroundImage: `url(${background})`,
+  
 };
+// const photo = {
+//   personal: `url(${personal})`,
+// }
 
 function Header() {
   return (
     <div style= {styles}>
       <div class="my_name ">Brad Tomlinson</div>
-      <img src=".\images\personal.png" alt="image of Brad Tomlinson" />
+      <img src= "./images/personal.png" alt="Brad Tomlinson" />
     </div>
   );
 }
