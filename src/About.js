18a8d10c1fb140ae8666e11f582df2f20d33e994
jcerb <brt.jst@gmail.com>
2021-04-27 16:30:14 -0700

    create about.js

---
+++ src/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const style = {
+    
+}
+
+function About() {
+    return (
+        <div>
+             <h1>About Brad Tomlinson</h1>
+      <p>
+        
+      </p>
+            
+        </div>
+    )
+}
+
+export default About
