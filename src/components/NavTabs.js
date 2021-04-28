68724c1dc36c0bb08ed5cbff0860ea1fcf0e0998
jcerb <brt.jst@gmail.com>
2021-04-27 17:26:30 -0700

    begin navtabs.js

---
+++ src/components/NavTabs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function NavTabs() {
+    return (
+        <div>
+            <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/about"
+          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </Link>
+      </li>
+      </ul>
+            
+        </div>
+    )
+}
+
+export default NavTabs
