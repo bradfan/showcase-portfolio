0b7bc5072664db970bc3e7f2ea2c4b9e44cb94eb
Brad <brt.jst@gmail.com>
2021-07-06 19:00:08 -0700

    adjust about page

--- src/components/About.js
+++ src/components/About.js
@@ -15,16 +15,21 @@ function About() {
         experiences; my work history and my accomplishments reflect my desire to
         learn and ability to contribute in a team environment. I am interested
         in working with a company that will allow me to grow with them, both
-        professionally and technically. I have a proven track record of
-        contributing to many organizations through perseverance, communication
-        and dedication to the tasks at hand; no job is too large or too small. When I
-        am not on job I love outdoor activities such as hiking, camping and
-        kayaking and when I am around the house I enjoy playing music, working
-        on my Jeep and barbequing meals for my family.
+        professionally and technically. I have recently studied JavaScript, ES6+, CSS3,
+        HTML5, SQL, NoSQL, MongoDB, MySQL Express, React, Node, Handlebars,
+        JQuery & Bootstrap and I continue to improve and learn more technologies.
       </p>
       <p>
-        I am not afraid to break a sweat or get my hands dirty; please contact
-        me if you would like to learn more about how I can be of benefit to your
+        I have a proven track record of contributing to many organizations
+        through perseverance, communication and dedication to the tasks at hand;
+        no job is too large or too small.When I am not on the job I love outdoor
+        activities such as hiking, camping and kayaking and when I am around the
+        house I enjoy playing music, working on my Jeep and barbequing meals for
+        my family.
+      </p>
+      <p>
+        I am not afraid to break a sweat or get my hands dirty. Contact
+        me if you would like to learn more about me and how I can be of benefit to your
         company.
       </p>
     </div>
