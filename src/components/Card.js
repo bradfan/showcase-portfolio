248f1c395543ad3000b31186ced8fb321db49eff
Brad <brt.jst@gmail.com>
2021-10-25 16:15:26 -0700

    format card css to index.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,73 +1,35 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 
 const screenWidth = 1;
 const cardWidth = screenWidth < 1100 ? "95%" : "45%";
 
-const styles = {
-  display: "flex",
-  flexDirection: "column",
-  flexWrap: "wrap",
-  width: cardWidth,
-  margin: "2%",
-  padding: ".5%",
-  minWidth: "300px",
-  border: "3px solid black",
-  borderRadius: "10px",
-  backgroundColor: "#FAEBD7",
-};
-const lineStyle = {
-  listStyleType: "none",
-   textAlign: "center",
-  color: "black",
-};
-const textStyle = {
-  width: "calc(100% - 200px)",
- 
-  margin: "0 auto",
-};
-
 function Card({ props }) {
   return (
     <div>
-      <div style={styles} className="card">
+      <div className="card">
         <div className="img-container">
-          <p style={lineStyle}>
+          <p className="line">
             <strong>Name: {props.name}</strong>
           </p>
-          <img
-            style={{
-              width: "200px",
-              height: "200px",
-              border: "1px solid black",
-              borderRadius: "10px",
-              display: "flex",
-              textAlign: "center",
-              margin:"0 auto",
-            }}
-            src={props.image}
-            alt={props.name}
-          />
+          <img className="img" src={props.image} alt={props.name} />
         </div>
-        <div style={textStyle}>
-          <ul style={
-            {padding:"0", textAlign: "center",}
-
-          }>
-            <li style={lineStyle}>
+        <div class="text">
+          <ul className="list">
+            <li className="line">
               <strong>Description: {props.description}</strong>
             </li>
-            <li style={lineStyle}>
+            <li className="line">
               <strong>Technologies: {props.technologies}</strong>
             </li>
-            <li style={lineStyle}>
+            <li className="line">
               <strong>
                 <a href={props.github} target="blank">
                   Click Here to view the Github Repo
                 </a>
               </strong>
             </li>
-            <li style={lineStyle}>
+            <li className="line">
               <strong>
                 <a href={props.deployed} target="blank">
                   Click Here to Deploy the Application
@@ -80,6 +42,5 @@ function Card({ props }) {
     </div>
   );
 }
-
 
 export default Card;
