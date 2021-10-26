b30e6f7f619e98afa6516f199bc0627705049dac
Brad <brt.jst@gmail.com>
2021-10-26 15:36:59 -0700

    add readability to about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,35 @@
 import React from "react";
 
-const styles = {
-  fontFamily: "Roboto, Times New Roman, Times, serif",
-  textAlign: "center",
-  backgroundColor: "#FAEBD7",
-};
-
 function About() {
   return (
-    <div style={styles}>
+    <div className="about-text">
       <h1>About Brad Tomlinson</h1>
+      <div className="about-text-box">
       <p>
         I have a wealth of attributes which I have earned through a lifetime of
         experiences; my work history and my accomplishments reflect my desire to
         learn and ability to contribute in a team environment. I am interested
         in working with a company that will allow me to grow with them, both
-        professionally and technically. I have recently studied JavaScript, ES6+, CSS3,
-        HTML5, SQL, NoSQL, MongoDB, MySQL, Express, React, Node, Handlebars,
-        JQuery & Bootstrap and I continue to improve and learn more technologies.
+        professionally and technically. I have recently studied JavaScript,
+        ES6+, CSS3, HTML5, SQL, NoSQL, MongoDB, MySQL, Express, React, Node,
+        Handlebars, JQuery & Bootstrap and I continue to improve and learn more
+        technologies.
       </p>
       <p>
         I have a proven track record of contributing to many organizations
         through perseverance, communication and dedication to the tasks at hand;
-        no job is too large or too small. When I am not on the job I love outdoor
-        activities such as hiking, camping and kayaking; when I am around the
-        house I enjoy playing music, working on my Jeep, home improvements and barbequing meals for
-        my family.
+        no job is too large or too small. When I am not on the job I love
+        outdoor activities such as hiking, camping and kayaking; when I am
+        around the house I enjoy playing music, working on my Jeep, home
+        improvements and barbequing meals for my family.
       </p>
       <p>
-        I am not afraid to break a sweat or get my hands dirty. Contact
-        me if you would like to learn more about me and how I can be of benefit to your
-        company.
+        I am not afraid to break a sweat or get my hands dirty. Contact me if
+        you would like to learn more about me and how I can be of benefit to
+        your company.
       </p>
+      </div>
+      
     </div>
   );
 }
