58d12beb41a18c293fb9105f135b0b735ed1704e
Brad <brt.jst@gmail.com>
2021-10-27 14:43:51 -0700

    add jsx to about

--- src/components/About.js
+++ src/components/About.js
@@ -2,34 +2,52 @@ import React from "react";
 
 function About() {
   return (
-    <div className="about-text">
-      <h1>About Brad Tomlinson</h1>
-      <div className="about-text-box">
-      <p>
-        I have a wealth of attributes which I have earned through a lifetime of
-        experiences; my work history and my accomplishments reflect my desire to
-        learn and ability to contribute in a team environment. I am interested
-        in working with a company that will allow me to grow with them, both
-        professionally and technically. I have recently studied JavaScript,
-        ES6+, CSS3, HTML5, SQL, NoSQL, MongoDB, MySQL, Express, React, Node,
-        Handlebars, JQuery & Bootstrap and I continue to improve and learn more
-        technologies.
-      </p>
-      <p>
-        I have a proven track record of contributing to many organizations
-        through perseverance, communication and dedication to the tasks at hand;
-        no job is too large or too small. When I am not on the job I love
-        outdoor activities such as hiking, camping and kayaking; when I am
-        around the house I enjoy playing music, working on my Jeep, home
-        improvements and barbequing meals for my family.
-      </p>
-      <p>
-        I am not afraid to break a sweat or get my hands dirty. Contact me if
-        you would like to learn more about me and how I can be of benefit to
-        your company.
-      </p>
+    <div class="about-box">
+      <div class="collage">
+        <img
+          src="img/nat-1-large.jpg"
+          alt="Photo 1"
+          class="collage_photo collage_photo--p1"
+        />
+        <img
+          src="img/nat-2-large.jpg"
+          alt="Photo 2"
+          class="collage_photo collage_photo--p2"
+        />
+        <img
+          src="img/nat-3-large.jpg"
+          alt="Photo 3"
+          class="collage_photo collage_photo--p3"
+        />
       </div>
-      
+      <div className="about-text">
+        <h1>About Brad Tomlinson</h1>
+        <div className="about-text-box">
+          <p>
+            I have a wealth of attributes which I have earned through a lifetime
+            of experiences; my work history and my accomplishments reflect my
+            desire to learn and ability to contribute in a team environment. I
+            am interested in working with a company that will allow me to grow
+            with them, both professionally and technically. I have recently
+            studied JavaScript, ES6+, CSS3, HTML5, SQL, NoSQL, MongoDB, MySQL,
+            Express, React, Node, Handlebars, JQuery & Bootstrap and I continue
+            to improve and learn more technologies.
+          </p>
+          <p>
+            I have a proven track record of contributing to many organizations
+            through perseverance, communication and dedication to the tasks at
+            hand; no job is too large or too small. When I am not on the job I
+            love outdoor activities such as hiking, camping and kayaking; when I
+            am around the house I enjoy playing music, working on my Jeep, home
+            improvements and barbequing meals for my family.
+          </p>
+          <p>
+            I am not afraid to break a sweat or get my hands dirty. Contact me
+            if you would like to learn more about me and how I can be of benefit
+            to your company.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
