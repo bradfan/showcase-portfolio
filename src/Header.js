53c515472cd375e67eeddee86da1ea041d304f16
Brad <brt.jst@gmail.com>
2021-12-01 10:17:55 -0700

    verify resume link header.js

--- src/Header.js
+++ src/Header.js
@@ -52,7 +52,7 @@ function Header() {
           </strong>
           <strong>
             <a
-              href="https://1drv.ms/w/s!Akf67WBgSDkQjRjlrneuojd7IkvV?e=j9EkMK"
+              href="https://1drv.ms/b/s!Akf67WBgSDkQjwvbXPLMzIa2Xe30?e=GfkElp"
               target="blank"
             >
               <li>Resume</li>
